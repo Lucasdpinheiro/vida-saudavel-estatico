f4df96daa6505fd0eea1f74ff52c28002242e03d
F6322043 Lucas Duarte Pinheiro <luc.dpinheiro@bb.com.br>
2022-10-06 17:23:15 -0300

    Add: Interfaces com as entidades da api.

---
+++ src/app/types.ts
@@ -0,0 +1,30 @@
+export interface User {
+    id?: number;
+    fullName: string;
+    birthDay: Date;
+    weight: number;
+    height: number;
+    sex: string;
+    activityLevel: string;
+    goal?: Goals;
+  }
+
+export interface Goals {
+    id?: number;
+    hydration?: number;
+    weightLoss?: number;
+}
+
+export interface HydrationRecord {
+    id?: number;
+    date: Date;
+    user: User;
+    waterIntake: number;
+}
+
+export interface WeightLossRecord {
+    id?: number;
+    date: Date;
+    user: User;
+    currentWeight: number;
+}
