89a446a03e58f8d2802c7699bf0fda76e91f1c86
F6322043 Lucas Duarte Pinheiro <luc.dpinheiro@bb.com.br>
2022-10-15 13:11:38 -0300

    Edit: Criando a função de sign in utilizando observables.

--- src/app/dataservice/UserService.ts
+++ src/app/dataservice/UserService.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../types';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private users: User[] = [];
+  private readonly API = 'http://localhost:8080/users/';
 
   constructor(private httpClient: HttpClient) {}
 
-  signIn () {}
-  createAccount () {}   
+  signIn(userId: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.API}` + userId);
+  }
+
+  createAccount() {
+  }
 }
