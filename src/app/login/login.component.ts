dc4049e95d63b91b19e4b3a1cf9be5774f5f58c7
F6322043 Lucas Duarte Pinheiro <luc.dpinheiro@bb.com.br>
2022-10-15 13:15:36 -0300

    Refact: Limpando o código da tela de login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../dataservice/UserService';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor(private userService: UserService) { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class LoginComponent { }
