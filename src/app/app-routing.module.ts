052271369c5a59eedf4d2a2626a7ab1bfe1520b2
F6322043 Lucas Duarte Pinheiro <luc.dpinheiro@bb.com.br>
2022-10-15 13:04:04 -0300

    Add: Adicionando os caminhos da página 'Home' e 'Page not found', além do redirecionamento do wildcard '**' para 'Page not found'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
-    pathMatch: 'full',
+    title: 'Login',
     component: LoginComponent
+  },
+  {
+    path: 'home',
+    title: 'Home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    title: 'Page not found',
+    component: PageNotFoundComponent
   }
 ];
 
